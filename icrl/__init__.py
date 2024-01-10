9af7013a7e91bd8f96851d7ccf07144fb61501be
quangr <quangr@163.com>
2024-01-10 18:19:52 +0800

    add maze env

---
+++ icrl/__init__.py
@@ -0,0 +1,21 @@
+from icrl.env.wrapper.mazewrapper import ConcatenateObservationNoGoal
+from gymnasium.envs.registration import register
+
+
+register(
+    id=f"Maze2d_simple",
+    entry_point="gymnasium_robotics.envs.maze.point_maze:PointMazeEnv",
+    kwargs={
+        "continuing_task":False,
+        "reward_type":"sparse",
+        "maze_map": [
+            [1, 1, 1, 1, 1],
+            [1, "g", 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, "r", 0, 0, 1],
+            [1, 1, 1, 1, 1],
+        ],
+    },
+    additional_wrappers=(ConcatenateObservationNoGoal.wrapper_spec(),),
+    max_episode_steps=300,
+)
